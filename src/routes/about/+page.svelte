c50efc4e651e16a2336ebd228ca366ae8a66fbce
Domenik Reitzner <domenik@reitzner.at>
2023-05-02 10:49:54 +0200

    add matches

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,9 +2,11 @@
   import { page } from "$app/stores";
 
   export let data;
+  $: matches = data.pathname === $page.url.pathname;
 </script>
 
 <div class="text-column">
-  <p>+page.server.js pathname: {data.pathname}</p>
-  <p>+page.svelte pathname: {$page.url.pathname}</p>
+	<p>matches: {matches}</p>
+	<p>+page.server.js pathname: {data.pathname}</p>
+	<p>+page.svelte pathname: {$page.url.pathname}</p>
 </div>
